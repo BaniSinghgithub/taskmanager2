2d7c8746e484621fff429654a02bc11f1cd451c0
BANI SINGH <141717536+BaniSinghgithub@users.noreply.github.com>
2025-04-07 00:36:58 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -30,4 +30,13 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received, shutting down gracefully...');
+  server.close(() => {
+    console.log('Server closed');
+    process.exit(0);
+  });
+});
+
+
 module.exports= app;
